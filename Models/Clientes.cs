6f5b3a56458831c63bbd67ea32547011457fbdf9
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-11-04 16:45:21 -0400

    Models Listas

---
+++ Models/Clientes.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Elvis_P2_AP2_2.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+
+        [ForeignKey("ClienteId")]
+        public virtual List<Ventas> venta { get; set; }
+    }
+}
+8
